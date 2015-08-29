6d6847497e538cb3602375b964a15f494dcd17a4
ksheffield3 <Kelley Sheffield>
2015-08-29 15:44:03 -0400

    Speed and Collision Reports

---
+++ data_generator.js
@@ -0,0 +1,83 @@
+function get_hard_brakes()
+{
+	if(document.getElementById("brake_time").value=="week")
+	{
+		document.getElementById("total_brakes").innerHTML = 2;
+	}
+	if(document.getElementById("brake_time").value=="month")
+	{
+		document.getElementById("total_brakes").innerHTML = 4;
+	}
+	if(document.getElementById("brake_time").value=="year")
+	{
+		document.getElementById("total_brakes").innerHTML = 5;
+	
+	}
+}
+
+
+function get_brake_avg()
+{
+	if(document.getElementById("brake_avg").value=="week")
+	{
+		document.getElementById("avg_brake").innerHTML = 0.5;
+	}
+	if(document.getElementById("brake_avg").value=="month")
+	{
+		document.getElementById("avg_brake").innerHTML = .02;
+	}
+	if(document.getElementById("brake_avg").value=="year")
+	{
+		document.getElementById("avg_brake").innerHTML = 0.1;
+	}
+}
+
+
+function get_speed_avg()
+{
+	if(document.getElementById("speed_avg").value=="week")
+	{
+		document.getElementById("avg_speed").innerHTML = "35 mph";
+	}
+	if(document.getElementById("speed_avg").value=="month")
+	{
+		document.getElementById("avg_speed").innerHTML = "46 mph";
+	}
+	if(document.getElementById("speed_avg").value=="year")
+	{
+		document.getElementById("avg_speed").innerHTML = "42 mph";
+	}	
+}
+
+function get_max_speed()
+{
+	if(document.getElementById("max_speed").value=="week")
+	{
+		document.getElementById("speed_max").innerHTML = "62 mph";
+	}
+	if(document.getElementById("max_speed").value=="month")
+	{
+		document.getElementById("speed_max").innerHTML = "75 mph";
+	}
+	if(document.getElementById("max_speed").value=="year")
+	{
+		document.getElementById("speed_max").innerHTML = "78 mph";
+	}	
+}
+
+
+function get_stab_report()
+{
+	if(document.getElementById("stab_report").value=="week")
+	{
+		document.getElementById("report_stab").innerHTML = "2 times";
+	}
+	if(document.getElementById("stab_report").value=="month")
+	{
+		document.getElementById("report_stab").innerHTML = "2 times";
+	}
+	if(document.getElementById("stab_report").value=="year")
+	{
+		document.getElementById("report_stab").innerHTML = "3 times";
+	}	
+}
